a33ec9cb00fa2bdfec630bf6832485a4646122f4
rohit_mishr <rohitmishra.rm583@gmail.com>
2026-08-21 17:09:44 +0530

    buttton polling and debounce logic test

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -1,20 +1,59 @@
 #include <stdint.h>
-#include "config.h"
 #include "main.h"
+#include "config.h"
 #include "usart.h"
+#include "button.h"
+#include "flash.h"
+#include "led.h"
 
+
+void delay(volatile uint32_t count) {
+    while(count--) {}
+}
 
 int main(void) {
     // 1. TODO: Initialize Clocks and UART
-    // bootloader_uart_init();
+    bootloader_uart_init();
+    button_init();
+    led_init();
+
+    // check for button press
+    uint32_t i = 0;
+    while (get_button_state() && i < 500000) { 
+        i++; 
+    }
+    
+    // 3. Evaluate the result
+    if (i < 500000) {
+        // Button was NOT held long enough (or not pressed at all)
+        while(1) {
+            char *s = "Not in bootloader mode\r\n";
+            int j = 0;
+            while (s[j] != '\0') {
+                usart_send_char(s[j++]); // (Assuming your TX function is named uart_tx)
+            }
+            delay(1000000); // Wait a bit before printing again
+        }
+    }
+    
+    // 4. Bootloader Mode (Button was held)
+    led_on();
+    while(1) {
+        char *s = "In bootloader mode\r\n";
+        int j = 0;
+        while (s[j] != '\0') {
+            usart_send_char(s[j++]);
+        }
+        delay(1000000); // Wait a bit before printing again
+    }
+    
+    
     
     // 2. TODO: Check if the user is requesting a firmware update
     
     // 3. TODO: De-initialize clocks and UART before jumping
-    
-    // 4. Jump to the main application
-    // Temporary Hardware Test Loop
-    jump_to_application();
+
+    // jump_to_application();
     
     // The CPU has left the bootloader. It will never reach this while loop.
     while (1) {
