07a2c16606f011dc1dfb8032bf46555b23a21e29
rohit_mishr <rohitmishra.rm583@gmail.com>
2026-08-22 16:42:04 +0530

    build: add application structure and CMake configuration

---
+++ application/src/startup.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "startup.h"
+
+/* 2. The Reset Handler (The very first function that runs) */
+void Reset_Handler(void) {
+    /* Step A: Copy the initialized variables (.data) from Flash to RAM */
+    uint32_t *src = &_sidata;
+    uint32_t *dest = &_sdata;
+    while (dest < &_edata) {
+        *dest++ = *src++;
+    }
+
+    /* Step B: Zero out the uninitialized variables (.bss) in RAM */
+    dest = &_sbss;
+    while (dest < &_ebss) {
+        *dest++ = 0;
+    }
+
+    /* Step C: Jump to the main application */
+    main();
+
+    /* Safety trap: if main() ever accidentally returns, freeze here */
+    while (1);
+}
+
+/* 3. The Vector Table */
+/* We use a compiler attribute to force this array into the ".isr_vector" section */
+__attribute__((section(".isr_vector")))
+uint32_t *vector_table[] = {
+    (uint32_t *)&_estack,       /* Address 0x00: The Initial Stack Pointer */
+    (uint32_t *)Reset_Handler   /* Address 0x04: The Reset Handler address */
+};
